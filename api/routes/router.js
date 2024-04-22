4195b2844d997d88fb6277d420fe53ea9a2f2d7c
kevinkim <kevinnkimm96@gmail.com>
2024-04-22 10:22:46 +0800

    remove unused route

--- api/routes/router.js
+++ api/routes/router.js
@@ -5,12 +5,10 @@ const chatRoutes = require("./chat");
 const historyRoutes = require("./history");
 const logoutRoute = require("./logout");
 const sessionRoutes = require("./session");
-const usersRouter = require("./users");
 
 router.use("/chat", chatRoutes);
 router.use("/history", historyRoutes);
 router.use("/logout", logoutRoute);
 router.use("/session", sessionRoutes);
-router.use("/users", usersRouter);
 
 module.exports = router;
